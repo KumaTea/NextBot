da3650bcedda2b3deffee0bcbeb278a39ac2abd6
KumaTea <KumaTea@outlook.com>
2024-01-17 13:36:21 +0800

    sync auth from TGBot

--- bot/auth.py
+++ bot/auth.py
@@ -1,33 +1,57 @@
 from typing import Union
 from pyrogram import Client
 from bot.session import logging
-from common.local import bl_users, known_group
 from pyrogram.types import Message, CallbackQuery
+from common.local import bl_users, known_group, known_user_ids
 
 
 def ensure_not_bl(func):
     async def wrapper(client: Client, obj: Union[Message, CallbackQuery]):
+
+        # target the message
         if isinstance(obj, CallbackQuery):
             msg = obj.message
         else:
             msg = obj
-        if msg.chat and msg.chat.id not in known_group:
+
+        # detect unknown group
+        if msg.chat and msg.chat.id < 0 and msg.chat.id not in known_group:
             logging.warning(f'Chat id={msg.chat.id} name={msg.chat.title} not known!')
             known_group.append(msg.chat.id)
+
+        # users
         if msg.from_user:
             user_id = msg.from_user.id
+            # blocked?
             if user_id in bl_users:
                 logging.warning(f'User {user_id} is in blacklist! Ignoring message.')
                 return None
+            # no pic?
+            user_pic = msg.from_user.photo
+            if not user_pic:
+                logging.warning(f'User {user_id} has no profile picture! Ignoring message.')
+                return None
+            # premium?
+            is_premium = msg.from_user.is_premium
+            is_known = user_id in known_user_ids.data
+            if is_premium and not is_known:
+                logging.warning(f'User {user_id} is a premium user! Ignoring message.')
+                return None
+
+        # replies
         if msg.reply_to_message and msg.reply_to_message.from_user:
             user_id = msg.reply_to_message.from_user.id
             if user_id in bl_users:
                 logging.warning(f'Replied user {user_id} is in blacklist! Ignoring message.')
                 return None
+
+        # forwards
         if msg.forward_from:
             user_id = msg.forward_from.id
             if user_id in bl_users:
                 logging.warning(f'Forwarded user {user_id} is in blacklist! Ignoring message.')
                 return None
+
+        # all good
         return await func(client, obj)
     return wrapper
--- common/tools.py
+++ common/tools.py
@@ -1,10 +1,57 @@
 import os
-from common.data import pwd
+import re
+from typing import Union
+from urllib.request import urlopen
+from common.data import url_regex, pwd
 
 
-def sort_imports():
+def trimmer(data: Union[dict, list]):
+    """
+    Remove empty items from dict or list.
+    """
+    if type(data) is dict:
+        new_data = {}
+        for key in data:
+            if data[key]:
+                new_data[key] = trimmer(data[key])
+        return new_data
+    elif type(data) is list:
+        new_data = []
+        for index in range(len(data)):
+            if data[index]:
+                new_data.append(trimmer(data[index]))
+        return new_data
+    else:
+        return data
+
+
+def trim_key(data: dict, char: str = '_'):
+    """
+    Remove keys that start with char,
+    defaults to '_'.
+    """
+    trim_list = []
+    for i in data:
+        if i.startswith(char):
+            trim_list.append(i)
+    for i in trim_list:
+        data.pop(i)
+    return data
+
+
+def find_url(text: str):
+    if text:
+        result = re.findall(url_regex, text)
+        if result:
+            return result[0]
+    return None
+
+
+# run this before commit
+# for aesthetic purpose
+def sort_imports(path: pwd):
     # python_files = [i for i in os.listdir() if i.endswith('.py')]
-    for root, dirs, files in os.walk(pwd):
+    for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith('.py'):
                 sort_import(os.path.join(root, file))
@@ -18,17 +65,67 @@ def sort_import(file):
     for line in lines:
         if line.startswith('import ') or line.startswith('from '):
             imports.append(line)
+        else:
+            break
+    if not imports:
+        return None
     imports.sort(key=lambda x: len(x.split('  #')[0]))
     for i in range(len(imports)):
         imports[i] = imports[i].rstrip() + '\n'
     for i in range(len(lines)):
         if lines[i].startswith('import ') or lines[i].startswith('from '):
             lines[i] = imports.pop(0)
+        if not imports:
+            break
     if lines != original_lines:
         with open(file, 'w', encoding='utf-8') as f:
             f.writelines(lines)
         print(f'{file} sorted')
 
 
+def get_url_text(url: str) -> str:
+    with urlopen(url) as response:
+        return response.read().decode('utf-8')
+
+
+def process_text(text: str) -> str:
+    text = text.replace(',', '')
+    text = text.replace('\t', '')
+    text = text.strip()
+    text = text.split('#')[0]
+    text = text.split(' ')[0]
+    return text
+
+
+def get_url_int_list(url: str) -> list[int]:
+    int_list = []
+    text = get_url_text(url)
+    for line in text.splitlines():
+        num_text = process_text(line)
+        if num_text:
+            num = int(num_text)
+            int_list.append(num)
+
+    return int_list
+
+
+def get_url_str_list(url: str) -> list[str]:
+    str_list = []
+    text = get_url_text(url)
+    for line in text.splitlines():
+        str_text = process_text(line)
+        if str_text:
+            str_list.append(str_text)
+
+    return str_list
+
+
+def get_url_str(url: str) -> str:
+    text = get_url_text(url)
+    while text.endswith('\n'):
+        text = text[:-1]
+    return text.strip()
+
+
 if __name__ == '__main__':
-    sort_imports()
+    sort_imports(r'D:\GitHub\TGBot')
